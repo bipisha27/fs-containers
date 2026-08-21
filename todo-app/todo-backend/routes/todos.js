da261215eb3aee71a9eaea62f09bd30c8824696f
Bipisha <paudelbipisha4@gmail.com>
2026-08-21 15:08:05 +0545

    exercise 7: implement GET and PUT

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,17 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo); // Implement this
 });
 
 /* PUT todo. */
 singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  const updatedTodo = await Todo.findByIdAndUpdate(
+	req.todo.id,
+	req.body,
+	{new: true}
+); // Implement this
+	res.send(updatedTodo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
