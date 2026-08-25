5f08725d30616dddc2e047c7bb2c205f8958687e
Bipisha <paudelbipisha4@gmail.com>
2026-08-25 12:57:27 +0545

    exercise 21

--- todo-app/todo-backend/app.js
+++ todo-app/todo-backend/app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todos');
-
+const redis = require('./redis');
 const app = express();
 
 app.use(cors());
@@ -14,5 +14,12 @@ app.use(express.json());
 
 app.use('/', indexRouter);
 app.use('/todos', todosRouter);
+
+app.get('/statistics', async(_, res) => {
+  const addedTodos = await redis.get('added_todos')
+  res.send({
+    added_todos: Number(addedTodos || 0)
+  })
+});
 
 module.exports = app;
